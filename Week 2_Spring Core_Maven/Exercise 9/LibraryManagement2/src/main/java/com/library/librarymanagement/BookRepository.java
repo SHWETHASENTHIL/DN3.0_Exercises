069ddc496303cb47f0151fcb5a572fcd880f8291
SHWETHA S <shwethasenthil@gmail.com>
2024-08-07 18:22:50 +0530

    Delete Week 2_Spring Core_Maven directory

--- Week 2_Spring Core_Maven/Exercise 9/LibraryManagement2/src/main/java/com/library/librarymanagement/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.library.librarymanagement;
-package com.example.librarymanagement;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
