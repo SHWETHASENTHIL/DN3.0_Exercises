79b11fe118aa973758a403914eda3f85617b6ab1
SHWETHA S <shwethasenthil@gmail.com>
2024-08-07 18:28:10 +0530

    Add files via upload

---
+++ Week 2_Spring Core_Maven/Exercise 9/LibraryManagement2/LibraryManagement2/src/main/java/com/library/LibraryManagement2/LibraryManagement2Application.java
@@ -0,0 +1,13 @@
+package com.library.LibraryManagement2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagement2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagement2Application.class, args);
+	}
+
+}
---
+++ Week 2_Spring Core_Maven/Exercise 9/LibraryManagement2/src/main/java/com/library/librarymanagement/BookController.java
@@ -0,0 +1,55 @@
+package com.library.librarymanagement;
+package com.example.librarymanagement;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> getBookById(@PathVariable Long id) {
+        Optional<Book> book = bookRepository.findById(id);
+        return book.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Book createBook(@RequestBody Book book) {
+        return bookRepository.save(book);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Book> updateBook(@PathVariable Long id, @RequestBody Book bookDetails) {
+        Optional<Book> book = bookRepository.findById(id);
+        if (book.isPresent()) {
+            Book existingBook = book.get();
+            existingBook.setTitle(bookDetails.getTitle());
+            existingBook.setAuthor(bookDetails.getAuthor());
+            existingBook.setIsbn(bookDetails.getIsbn());
+            return ResponseEntity.ok(bookRepository.save(existingBook));
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteBook(@PathVariable Long id) {
+        if (bookRepository.existsById(id)) {
+            bookRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+}
